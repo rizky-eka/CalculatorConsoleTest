07c72e2c310b29565b12026555da5719ba1050fa
Rizky Eka Haryadi <127827977+rizky-eka@users.noreply.github.com>
2023-10-04 11:58:55 +0700

    Add files via upload

---
+++ 01 latihan/CalculatorConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculatorMachine;
+
+namespace CalculatorConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Demo Unit Testing Menggunakan App Console";
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("Hasil Penambahan 2 + 3 = {0}", cal.Penambahan(2,3));
+            Console.WriteLine("Hasil Pengurangan 7 - 3 = {0}", cal.Pengurangan(7, 3));
+            Console.WriteLine("Hasil Perkalian 5 x 2 = {0}", cal.Perkalian(5, 2));
+            Console.WriteLine("Hasil Pembagian 6 : 2 = {0}", cal.Pembagian(6, 2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 01 latihan/CalculatorUnitTest/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+using NUnit.Framework;
+using CalculatorMachine;
+
+namespace CalculatorUnitTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void PenambahanTest()
+        {
+            Assert.AreEqual(5, calculator.Penambahan(2, 3));
+        }
+        [Test]
+        public void PenguranganTest()
+        {
+            Assert.AreEqual(4, calculator.Pengurangan(7, 3));
+        }
+        [Test]
+        public void PerkalianTest()
+        {
+            Assert.AreEqual(10, calculator.Perkalian(5, 2));
+        }
+        [Test]
+        public void PembagianTest()
+        {
+            Assert.AreEqual(3, calculator.Pembagian(6, 2));
+        }
+
+    }
+}
